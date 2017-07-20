7fd75ecd224855a92f6274c9ef96cc644f5081dc
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-07-20 14:59:28 -0700

    display new posts inside new component with route path defined, introduces Switch from react reouter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=PAPERCLIP1234';
+const API_KEY = '?key=Zebra1234';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>
+        Postsnew!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
