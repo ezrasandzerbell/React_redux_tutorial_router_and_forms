575b564fed8f4c0073a96598c074f1c327a73dc1
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-07-21 12:28:50 -0700

    improve error messages on user input form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,15 +4,25 @@ import { Field, reduxForm } from 'redux-form';
 class PostsNew extends Component {
   renderField(field) {
 
+    const { meta: { touched, error } } = field;
+
+    // using destructuring to pull "touch" and "error" properties off of the nested "meta" object
+
+    const className = `form-group ${touched && error ? 'has-danger': ''}`
+
+    // colon is being used to signify "otherwise", as in if these two conditions, then has-danger, otherwise empty string
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error: ''}
+        </div>
       </div>
     );
   }
