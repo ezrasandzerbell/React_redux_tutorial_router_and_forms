cb0eba9f1f06eb6ead7adc6555b4a7487696cc0e
ezra sandzer-bell <equilibriumarts@gmail.com>
2017-07-21 15:56:58 -0700

    link list of props to individual post show pages, with link back to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions'
+import { Link } from 'react-router-dom';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
 
@@ -13,11 +14,12 @@ class PostsShow extends Component {
     const { post } = this.props;
 
     if(!post) {
-      return <div>Loading...</div>  
+      return <div>Loading...</div>
     }
 
     return (
       <div>
+      <Link to="/">Back to Index</Link>
         <h3> {post.title} </h3>
         <h6> Categories: {post.categories}</h6>
         <p>{post.content}</p>
